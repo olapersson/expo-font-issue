662b736b4f62865284ca4d2c4d7517455f7f57f2
Ola Persson <ola.persson@gmail.com>
2020-08-17 16:47:33 +0200

    add reproduction where react-native-screens/native-stack does not recognizing expo loaded fonts

--- App.tsx
+++ App.tsx
@@ -1,21 +1,55 @@
+import { enableScreens } from 'react-native-screens';
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { useFonts, Inter_900Black } from '@expo-google-fonts/inter';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
+// import {createStackNavigator} from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native';
+
+enableScreens();
+
+// const { Navigator, Screen } = createStackNavigator();
+const { Navigator, Screen } = createNativeStackNavigator();
+
+const Home = () => <View style={styles.container}>
+    <Text style={{fontFamily: 'Inter_900Black'}}>Open up App.tsx to start working on your app!</Text>
+    <StatusBar style="auto" />
+</View>;
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [fontsLoaded] = useFonts({
+        Inter_900Black
+    });
+
+    if (!fontsLoaded) {
+        return <View style={{flex: 1}}>
+            <Text>Loading</Text>
+        </View>;
+    }
+
+    return (
+        <NavigationContainer>
+            <Navigator initialRouteName="home">
+                <Screen
+                    options={{
+                        headerTitleStyle: {
+                            fontFamily: 'Inter_900Black'
+                        }
+                    }}
+                    name="home"
+                    component={Home}/>
+            </Navigator>
+        </NavigationContainer>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
